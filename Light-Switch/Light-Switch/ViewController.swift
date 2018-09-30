3a621759a40dbb01c2acb4075eb23e8c0593622e
debratton <debratton@gmail.com>
2018-09-30 16:21:07 -0500

    project udpated

--- Light-Switch/Light-Switch/ViewController.swift
+++ Light-Switch/Light-Switch/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isOn: Bool = true
+    
+    @IBOutlet weak var LightTextLbl: UILabel!
+    @IBOutlet weak var viewBackGround: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func lightsOnOffBtn(_ sender: Any) {
+        
+        if isOn == true {
+            LightTextLbl.text = "Lights Off"
+            LightTextLbl.textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            viewBackGround.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            isOn = false
+        } else {
+            LightTextLbl.text = "Lights On"
+            LightTextLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            viewBackGround.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            isOn = true
+        }
+        
+        
+    }
+    
 
 }
 
